c72993cb57196bce6e5f0e32ff09de03137b3edd
Sunil Harde <sunilharde10@gmail.com>
2024-07-13 17:52:30 +0530

    Navbar Addedt

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Navbar/Navbar.jsx'
+
+
+function App() {
+  return (
+    <div className='bg-dark vh-min-100'>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Switch, FilledInput } from '@mui/material'
+import './Navbar.css'
+function NavBar() {
+
+    const [navClick,setNavClick]=useState(true)
+
+
+    return (
+        <div>
+            <div className='container '>
+
+                <div className=  {navClick ?"nav d-flex  align-items-center justify-content-between text-light" : " nav-click d-flex  align-items-center justify-content-between text-light"}>
+                    <div className="logo d-flex">
+                        <h4>Web <span className='text-info'>Dev</span></h4>
+
+                    </div>
+
+                    <div className="nav-items text-bg-dark d-flex flex-column flex-md-row  align-items-md-center  justify-content-center gap-4 ">
+                        <p>Home</p>
+                        <p>Skill</p>
+                        <p>Projects</p>
+                        <p>Contact</p>
+                    </div>
+
+                        <div className="lines d-flex d-md-none flex-column" onClick={()=>setNavClick(!navClick)} >
+                            <div className="line"></div>
+                            <div className="line"></div>
+                        </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
