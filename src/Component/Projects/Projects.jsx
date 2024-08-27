f1b85fb40a21905261d425c60b99cb0f89cec01b
Sunil Harde <sunilharde10@gmail.com>
2024-08-27 16:23:30 +0530

    Projects Added

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,59 +1,54 @@
-import React from 'react'
-import Border from '../../Sub_Component/Line/Border'
-import ProjectsData from './ProjectsData'
-import "./projects.css"
-
-
+import React from 'react';
+import Border from '../../Sub_Component/Line/Border';
+import ProjectsData from './ProjectsData';
+import './projects.css';
 
 function Projects() {
     return (
-        <div className='container-fluid  skills-main '>
+        <div className='container-fluid skills-main'>
             <div className="main-heading d-flex flex-column align-items-center justify-content-center text-light mb-4">
-                <h1 className=' my-3'>Projects</h1>
+                <h1 className='my-3'>Projects</h1>
                 <h4>My <span>Projects</span></h4>
             </div>
-            <div className="projects row ">
-
-
+            <div className="projects row">
                 <div className="col-1">
-                    <div className='left-border1 '>
+                    <div className='left-border1'>
                         <Border />
                     </div>
                 </div>
 
-                <div className="cards col-12 col-md-10 ">
-                    <div className="row ">
-                        {
-                            ProjectsData.map((data) => (
-                                <div key={data.id} className=" col-12 col-sm-6 col-md-4 col-lg-3  ">
-                                    <div key={data.id} className="card mb-4 ">
-                                        <div className='card-header '>
-                                            <h3 className='mt-1'>{data.projectName}</h3>
+                <div className="cards col-12 col-md-11 col-lg-10">
+                    <div className="row">
+                        {ProjectsData.map((data) => (
+                            <div key={data.id} className="col-12 col-sm-6 col-md-4 col-lg-4 mb-4">
+                                <div className="card">
+                                    <div className='card-header'>
+                                        <h3 className='mt-1'>{data.projectName}</h3>
+                                    </div>
+
+                                    <div className='card-body py-0'>
+                                        <img src={data.projectIMG} alt={data.projectName} />
+                                        <p>{data.projectDetails}</p>
+                                        <div className="lag row">
+                                            {data.usedLanguage.map((language, index) => (
+                                                <div key={index} className='col-4 col-xs-3 col-md-4 col-lg-3'>
+                                                    <p  className=' px-1 border border-info rounded-3'>{language}</p>
+                                                </div>
+                                            ))}
                                         </div>
+                                    </div>
 
-                                        <div className='card-body'>
-                                            <img src={data.projectIMG} alt="" />
-                                            <p>{data.projectDeatils}</p>
-                                        </div>
-
-                                        <div className="card-footer">
-                                            <p>Visit Website :-<a href={data.projectLink}>{data.projectLink}</a></p>
-                                        </div>
-
-
+                                    <div className="card-footer">
+                                        <a href={data.projectLink} >Visit Website</a>
                                     </div>
                                 </div>
-                            ))
-                        }
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
-
-
-        </div >
-
-
-    )
+        </div>
+    );
 }
 
-export default Projects
+export default Projects;
--- src/Component/Projects/ProjectsData.js
+++ src/Component/Projects/ProjectsData.js
@@ -1,35 +1,30 @@
 const ProjectsData = [
     {
-        "key": 1,
+        "id": 1,
         "projectName": "To-Do",
-        "projectIMG": 90,
-        "projectDeatils": " with anrmed with an arsenal of technologies including HTML, CSS, JavaScript, React.js and, Bootstrap I embark on journeys through code to ",
-        "projectLink": " https://todo-list-523j.onrender.com/ "
+        "projectIMG": "path/to/todo-image.jpg",  // Update with the correct image URL or path
+        "projectDetails": "Armed with an arsenal of technologies including HTML, CSS, JavaScript, React.js, and Bootstrap, I embark on journeys through code to create functional and stylish applications.",
+        "usedLanguage": ["React", "CSS", "Bootstrap", "Firebase"],  // Array of languages
+        "projectLink": "https://todo-list-523j.onrender.com/"
     },
 
     {
-        "key": 2,
+        "id": 2,
         "projectName": "Weather-App",
-        "projectIMG": 80,
-        "projectDeatils": " with anrmed with an arsenal of technologies including HTML, CSS, JavaScript, React.js and, Bootstrap I embark on journeys through code to ",
-        "projectLink": "https://moviesinfos.netlify.app/ "
-    },
-  {
-        "key": 2,
-        "projectName": "Weather-App",
-        "projectIMG": 80,
-        "projectDeatils": " with anrmed with an arsenal of technologies including HTML, CSS, JavaScript, React.js and, Bootstrap I embark on journeys through code to ",
-        "projectLink": "https://moviesinfos.netlify.app/ "
+        "projectIMG": "path/to/weather-app-image.jpg",  // Update with the correct image URL or path
+        "projectDetails": "This is a simple application that shows current weather conditions for any location. This project helped me improve my skills in managing state and making API calls with React.",
+        "usedLanguage": ["React", "CSS", "Bootstrap"],
+        "projectLink": "https://moviesinfos.netlify.app/"
     },
 
     {
-        "key": 3,
-        "projectName": "Movies Deatil",
-        "projectIMG": 70,
-        "projectDeatils": " with anrmed with an arsenal of technologies including HTML, CSS, JavaScript, React.js and, Bootstrap I embark on journeys through code to ",
-        "projectLink": " https://weather-16ms.onrender.com/ "
+        "id": 3,
+        "projectName": "Movies Detail",
+        "projectIMG": "path/to/movies-detail-image.jpg",  // Update with the correct image URL or path
+        "projectDetails": "This is a simple website that shows movie details and also recommends popular, latest movies. This project helped me improve my skills in API calls with React.",
+        "usedLanguage": ["React", "CSS"],
+        "projectLink": "https://weather-16ms.onrender.com/"
     },
+];
 
-]
-
-export default ProjectsData
+export default ProjectsData;
