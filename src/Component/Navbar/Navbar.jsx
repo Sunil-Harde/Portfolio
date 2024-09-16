9770867200ca977c57b24761705f781a639d7527
Sunil Harde <sunilharde10@gmail.com>
2024-09-16 17:11:39 +0530

    darkmode buttob added

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,11 +4,16 @@ import './Navbar.css'
 function NavBar() {
 
     const [navClick, setNavClick] = useState(true)
+    const [darkmode, setDarkMode] = useState(false)
+
+    function darkmodeon (){
+        setDarkMode(!darkmode)
+    }
 
 
     return (
         <div>
-            <div className=''>
+            <div className={darkmode? "litemode":""}>
 
                 <div className={navClick ? "nav shadow-sm d-flex fixed-top px-5 bg-dark align-items-center justify-content-between fixed-top text-light" : " nav-click shadow-sm d-flex fixed-top px-5 bg-dark align-items-center justify-content-between fixed-top text-light"}>
 
@@ -16,12 +21,21 @@ function NavBar() {
                         <h4>Web <span className='text-info'>Dev</span></h4>
                     </div>
 
-                    <div className="nav-items text-bg-dark d-flex flex-column flex-md-row  align-items-md-center gap-4 ">
-                        <p onClick={()=>window.scrollTo(0, 0)} >Home</p>
-                        <p onClick={()=>window.scrollTo(0, 550)}>Skill</p>
-                        <p onClick={()=>window.scrollTo(0, 1220)}>Projects</p>
+                    <div className="nav-items bg-dark d-flex flex-column flex-md-row  align-items-md-center gap-4 ">
+                        <p onClick={() => window.scrollTo(0, 0)} >Home</p>
+                        <p onClick={() => window.scrollTo(0, 550)}>Skill</p>
+                        <p onClick={() => window.scrollTo(0, 1220)}>Projects</p>
                         <p>Contact</p>
                     </div>
+
+
+                    <div className={'darkmode-button position-fixed '} >
+
+                        <button className="darkmode" onClick={() => darkmodeon()}>
+
+                        </button>
+                    </div>
+
 
                     <div className="lines d-flex d-md-none flex-column" onClick={() => setNavClick(!navClick)} >
                         <div className="line"></div>
